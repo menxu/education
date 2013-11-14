6cea57ad5d3732a414d5217e6208ffde990a0cae
menxu <menxu_work@163.com>
2013-11-14 15:16:48 +0800

    education imitate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Education::Application.config.secret_token = '4c8036a3647dc4581c650889a3ad1d22c258765105000185dc67da3aedfb2ca7cdb099988d37d40cc3e8babf8f364644aa294f99970d84b971548adf95e01883'
